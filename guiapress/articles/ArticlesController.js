342f7756c187acd0c3555fb12269301389341fb3
pedrol2301 <ppriqq@gmail.com>
2021-09-29 17:07:03 -0300

    Inicio paginação

--- guiapress/articles/ArticlesController.js
+++ guiapress/articles/ArticlesController.js
@@ -56,5 +56,65 @@ router.post("/articles/delete", (require,response)=>{
         response.redirect("/admin/articles");
     }
 });
+router.get("/admin/articles/edit/:id", (require,response) =>{
+    var id = require.params.id
+    if (isNaN(id)) {
+        response.redirect("/admin/articles");
+    }
+    Article.findByPk(id).then((article) =>{
+        if (article != undefined) {
+            Category.findAll().then(categories =>{
+                response.render("admin/articles/edit",{
+                    article:article
+                    ,categories:categories
+                });
+            });
+        }else{
+            response.redirect("/admin/articles");
+        }
+    }).catch((erro)=>{
+        console.log(erro)
+        response.redirect("/admin/articles");
+    })
+});
+router.post("/articles/update", (require,response)=>{
+    var id = require.body.id;
+    var title = require.body.title;
+    var body = require.body.body;
+    var categoryId = require.body.category;
+    if (id !=undefined) {
+        if (!isNaN(id)) {
+            Article.update({title:title,slug:slugify(title),body:body,categoryId:categoryId},{
+                where:{id:id}
+            }).then(()=>{
+                response.redirect("/admin/articles");
+            }).catch(err =>{
+                response.redirect("/admin/articles");
+            })
+        }else{
+            response.redirect("/admin/articles");
+        }
+    }else{
+        response.redirect("/admin/articles");
+    }
+});
+router.get('/articles/page/:num',(require,response)=>{
+
+    var  page = require.params.num;
+
+    var offset = 0;
+
+    if (isNaN(page) || page==1) {
+        offset = 0;
+    }else{
+        offset = parseInt(page) * 4;
+    }
+    Article.findAndCountAll({
+        limit: 4
+        ,offset:offset
+    }).then(articles =>{
+        response.json(articles);
+    });
+});
 
 module.exports = router;
