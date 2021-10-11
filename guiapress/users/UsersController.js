156ce4e5f0e9f4b97892434350a2e7c604d30ea4
pedrol2301 <ppriqq@gmail.com>
2021-10-11 18:23:00 -0300

    Commit

--- guiapress/users/UsersController.js
+++ guiapress/users/UsersController.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../users/Users");
 
-
+router.get("/admin/users", (require,response) =>{
+    response.render("/users/index");
+});
 
 module.exports = router;
