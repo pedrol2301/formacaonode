a7af84b1719bb90e48d8592ae4636928b5926860
pedrol2301 <ppriqq@gmail.com>
2021-12-14 17:08:56 -0300

    Update promisses.js

--- ProgAssincrona/promisses.js
+++ ProgAssincrona/promisses.js
@@ -28,14 +28,25 @@ function enviarEmail(corpo, para){
 }
 
 
+async function _acao() {
+    var iduser = await pegarId();
+    var emailuser = await buscarEmailNoBanco(iduser);
+    enviarEmail("Olá, como vai?",emailuser).then(() => {
+        console.log("Email enviado, para o usuário com id: " + iduser)
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
 console.log("Inicio!");
-pegarId().then((id) => {
-    buscarEmailNoBanco(id).then((email) => { 
-        enviarEmail("Olá, como vai?",email).then(() => {
-            console.log("Email enviado, para o usuário com id: " + id)
-        }).catch(err => {
-            console.log(err);
-        })
-    })
-})
+// pegarId().then((id) => {
+//     buscarEmailNoBanco(id).then((email) => { 
+//         enviarEmail("Olá, como vai?",email).then(() => {
+//             console.log("Email enviado, para o usuário com id: " + id)
+//         }).catch(err => {
+//             console.log(err);
+//         })
+//     })
+// })
+_acao();
 console.log("FIM");
