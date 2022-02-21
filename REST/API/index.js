1d3c26f6c21502542a6d3aae1c377630abc1b169
pedrol2301 <ppriqq@gmail.com>
2022-02-21 13:04:46 -0300

    Fim API REST

--- REST/API/index.js
+++ REST/API/index.js
@@ -2,105 +2,199 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+
+
+const JWTSecret = "djkshahjksdajksdhasjkdhasjkdhasjkdhasjkd";
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+
+    if(authToken != undefined){
+
+        const bearer = authToken.split(' ');
+        var token = bearer[1];
+
+        jwt.verify(token,JWTSecret,(err, data) => {
+            if(err){
+                res.status(401);
+                res.json({err:"Token inválido!"});
+            }else{
+
+                req.token = token;
+                req.loggedUser = {id: data.id,email: data.email};
+                req.empresa = "Guia do programador";                
+                next();
+            }
+        });
+    }else{
+        res.status(401);
+        res.json({err:"Token inválido!"});
+    } 
+}
 
 var DB = {
     games: [
         {
-            id:1,
-            title:"COD",
+            id: 23,
+            title: "Call of duty MW",
             year: 2019,
-            price:60
+            price: 60
         },
         {
-            id:2,
-            title:"BF",
-            year: 2019,
-            price:70
+            id: 65,
+            title: "Sea of thieves",
+            year: 2018,
+            price: 40
         },
         {
-            id:3,
-            title:"F1",
-            year: 2020,
-            price:100
+            id: 2,
+            title: "Minecraft",
+            year: 2012,
+            price: 20
+        }
+    ],
+    users: [
+        {
+            id: 1,
+            name: "Victor Lima",
+            email: "victordevtb@guiadoprogramador.com",
+            password: "nodejs<3"
+        },
+        {
+            id: 20,
+            name: "Guilherme",
+            email: "guigg@gmail.com",
+            password: "java123"
         }
     ]
-};
+}
 
-app.get("/games", (req,res) =>{
+app.get("/games",auth,(req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
 });
-app.get("/game/:id", (req,res) =>{
 
-    var result = DB.games.find(element=> element.id == req.params.id);
-    
-    if (!result) {
-        res.statusCode = 404;
-        res.json({});
+app.get("/game/:id",auth,(req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
     }else{
-        res.statusCode = 200;
-        res.json(result);
+        
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+            res.statusCode = 200;
+            res.json(game);
+        }else{
+            res.sendStatus(404);
+        }
     }
 });
 
-app.post("/game",(req,res)=>{
-    var {title, price, year} = req.body
-
+app.post("/game",auth,(req, res) => { 
+    var {title, price, year} = req.body;
     DB.games.push({
-        id:4,
+        id: 2323,
         title,
         price,
         year
     });
+    res.sendStatus(200);
+})
 
-    res.sendStatus(200)
-
-});
-app.delete("/game/:id",(req,res)=>{
-
-    var result = DB.games.findIndex(element=> element.id == req.params.id);
-    
-    if (result == -1) {
-        res.sendStatus(404);
+app.delete("/game/:id",auth,(req, res) => {
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
     }else{
-        DB.games.splice(result,1);
-        res.sendStatus(200);
+        var id = parseInt(req.params.id);
+        var index = DB.games.findIndex(g => g.id == id);
+
+        if(index == -1){
+            res.sendStatus(404);
+        }else{
+            DB.games.splice(index,1);
+            res.sendStatus(200);
+        }
+    }
+});
+
+app.put("/game/:id",(req, res) => {
+
+    if(isNaN(req.params.id)){
+        res.sendStatus(400);
+    }else{
+        
+        var id = parseInt(req.params.id);
+
+        var game = DB.games.find(g => g.id == id);
+
+        if(game != undefined){
+
+            var {title, price, year} = req.body;
+
+            
+            if(title != undefined){
+                game.title = title;
+            }
+
+            if(price != undefined){
+                game.price = price;
+            }
+
+            if(year != undefined){
+                game.year = year;
+            }
+            
+            res.sendStatus(200);
+
+        }else{
+            res.sendStatus(404);
+        }
     }
 
 });
 
-app.put("/game/:id",(req,res)=>{
 
-    
-    var result = DB.games.find(element=> element.id == req.params.id);
-    
-    if (!result) {
-        res.statusCode = 404;
-        res.json({});
+app.post("/auth",(req, res) => {
+
+    var {email, password} = req.body;
+
+    if(email != undefined){
+
+        var user = DB.users.find(u => u.email == email);
+        if(user != undefined){
+            if(user.password == password){
+                jwt.sign({id: user.id, email: user.email},JWTSecret,{expiresIn:'48h'},(err, token) => {
+                    if(err){
+                        res.status(400);
+                        res.json({err:"Falha interna"});
+                    }else{
+                        res.status(200);
+                        res.json({token: token});
+                    }
+                })
+            }else{
+                res.status(401);
+                res.json({err: "Credenciais inválidas!"});
+            }
+        }else{
+            res.status(404);
+            res.json({err: "O E-mail enviado não existe na base de dados!"});
+        }
+
     }else{
-        var {title, price, year} = req.body;
-
-        if (title != undefined) {
-            result.title = title;
-        }
-        if (price != undefined) {
-            result.price = price;
-        }
-        if (year != undefined) {
-            result.year = year;
-        }
-        res.sendStatus(202);
-
+        res.status(400);
+        res.send({err: "O E-mail enviado é inválido"});
     }
-
-
 });
 
-app.listen(8085,() =>{
-    console.log("👍👌");
+app.listen(45679,() => {
+    console.log("API RODANDO!");
 });
