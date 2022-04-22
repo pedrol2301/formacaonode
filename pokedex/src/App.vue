dad2a59fd40361d4a33f2e674735117cb3f129dc
pedrol2301 <ppriqq@gmail.com>
2022-04-22 15:04:12 -0300

    Pokedex

---
+++ pokedex/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  created: function () {
+    console.log("teste");
+    console.log(axios);
+  }
+}
+</script>
+
+<style>
+
+</style>
