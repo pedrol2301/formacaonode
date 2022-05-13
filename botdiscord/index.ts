eb7852732f0fe13bf949fcc0a5ee26d1ba421a80
pedrol2301 <ppriqq@gmail.com>
2022-05-13 17:59:10 -0300

    Bot

--- botdiscord/index.ts
+++ botdiscord/index.ts
@@ -1,13 +1,23 @@
-import DiscordJs, { Intents } from 'discord.js';
+import DiscordJs, { Intents, User } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+var xingamento = [
+                    "Vai tomar no cu maluco",
+                    "🖕🏾",
+                    "Vai po caralho",
+                    "Bota dentadura no cu e ri pro caralho"
+                ];
+
+
 
 const client = new DiscordJs.Client({
     intents : [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
-    ]
+    ],
+    
 });
 
 client.on("ready",()=>{
@@ -20,6 +30,11 @@ client.on('messageCreate',(message)=>{
             content:"Testado patrão"
         })
     }
-})
+    let random = Math.floor(Math.random() * xingamento.length);
+
+    if (message.mentions.has(client.user.id)) {
+        message.reply(`${xingamento[random]}`);
+    }
+});
 
 client.login(process.env.TOKEN);
