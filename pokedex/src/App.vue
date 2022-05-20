8666c68c64a8bdb262e39a8b6278051a0cf98c48
pedrol2301 <ppriqq@gmail.com>
2022-05-20 17:24:41 -0300

    Pokedex

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <div id="app">
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <PokemonC :name="poke.name" :url="poke.url" :num="index +1" />
+    </div>
   </div>
 </template>
 
 <script>
 
 import axios from 'axios';
+import PokemonC from './components/Pokemon.vue';
 
 export default {
   name: 'App',
-  components: {
+  data(){
+    return {
+      pokemons: []
+    }
   },
+  components: {
+    PokemonC
+},
   created: function () {
-    console.log("teste");
-    console.log(axios);
+      axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+        console.log("OK");
+        this.pokemons = res.data.results;
+        console.log(this.pokemons)
+      });
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
---
+++ pokedex/src/components/Pokemon.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h1>{{num}} - {{upper}}</h1>
+        <small>{{url}}</small>
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    created: function(){
+        axios.get(this.url).then(res =>{
+            this.pokemon.type = res.data.types[0].name;
+            this.pokemon.front = res.data.sprites.front_default;
+            this.pokemon.back = res.data.sprites.back_default;
+            console.log(this.pokemon);
+        })
+    },
+    data(){
+        return {
+            pokemon :{}
+        }
+    },
+    props:{
+        num:Number,
+        name: String,
+        url : String
+    }
+    ,computed:{
+        upper : function(){
+            var val = this.name[0].toUpperCase() + this.name.slice(1);
+            
+            return val;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
