a76ce677fc37928b23a7a296ac24be745e6db9b0
pedrol2301 <ppriqq@gmail.com>
2022-07-26 17:04:56 -0300

    Admindash e WS

---
+++ WS/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+io.on("connection",(socket) =>{
+    console.log(socket);
+    console.log(socket.id);
+})
+
+app.set("view engine", "ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index");
+});
+
+http.listen(3000, ()=>{
+    console.log('👌');
+})
