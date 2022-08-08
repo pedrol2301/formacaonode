74b587d70c3ded54a9a1e8d03fc304163e181b6e
pedrol2301 <ppriqq@gmail.com>
2022-08-08 17:03:12 -0300

    ss

--- Mongo/index.js
+++ Mongo/index.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
+const articleModel = require("./article");
 
 mongoose.connect("mongodb://localhost:27017/aprendendomongo");
 
+const Article = mongoose.model("Article",articleModel);
+
+const artigo = new Article({
+    title:"Teste",
+    author:"AAAAA",
+    body:"asdmasimdkamsd asi dias idjsoai joisja oidjoisa jdoijs aas."
+});
+
+artigo.save();
