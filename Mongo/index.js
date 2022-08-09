458dfbebc900c0d5f99e60c73174580a98c6eb99
pedrol2301 <ppriqq@gmail.com>
2022-08-09 17:02:38 -0300

    a

--- Mongo/index.js
+++ Mongo/index.js
@@ -5,10 +5,26 @@ mongoose.connect("mongodb://localhost:27017/aprendendomongo");
 
 const Article = mongoose.model("Article",articleModel);
 
-const artigo = new Article({
-    title:"Teste",
-    author:"AAAAA",
-    body:"asdmasimdkamsd asi dias idjsoai joisja oidjoisa jdoijs aas."
-});
+/* INSERT NO MONGO */
+// const artigo = new Article({
+//     title:"Teste 3",
+//     author:"CCCCCCCCCC",
+//     body:"asdmasimdkamsd asi dias idjsoai joisja oidjoisa jdoijs aas.",
+//     special:false
+// });
 
-artigo.save();
+// artigo.save().then(
+//     console.log("Saved")
+// ).catch((error)=>{
+//     console.log(error)
+// });
+
+/* SELECT NO MONGO*/
+Article.find({}).then(articles =>{
+    console.log(articles)
+}).catch(er =>{
+    console.log(err)
+})
+
+
+// Article.findByIdAndDelete("62f265819d8cf3530ae2d19a").then(()=>{console.log("Dado Removido!")}).catch(err => console.log(err))
